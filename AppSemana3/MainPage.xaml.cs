ba42a0209a5b0648a521446372a0239021a9df21
DELL <DELL@DESKTOP-5I69DTD>
2022-04-24 02:13:27 -0500

    Agregar archivos de proyecto.

---
+++ AppSemana3/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppSemana3
+{
+    public partial class MainPage : ContentPage
+    {
+        public static String NombreUsuario = "estudiante2022";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUser.Text;
+            string clave = txtPass.Text;
+
+            if ((usuario == "estudiante2022") && (clave == "uisrael2022"))
+            {
+                await Navigation.PushAsync(new Pagina2(usuario, clave));
+            }
+            else
+            {
+                lblLogin.Text = "El usuario o contraseña es incorrecto";
+            }
+
+
+
+
+        }
+    }
+}
